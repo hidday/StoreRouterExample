b0bc4b3db12091b31cd08ffdc094de89c3c6b7ba
hidday <hidday@trafficpoint.io>
2019-03-03 15:38:29 +0200

    ongoing

--- routes/stores.js
+++ routes/stores.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const brandController = require('../../server/controllers/brand');
-import myFunc from './myfile';
+import * as storeController from 'controllers/Store';
 
 
 /* GET store requests */
-router.get('/api/v1/campaigns', brandController.index);
-router.get('/api/v1/storeId/:storeId/giftCard', brandController.index);
-router.get('/api/v1/settings', brandController.index);
+router.get('/api/v1/campaigns', storeController.handleCampaignsRequest);
+router.get('/api/v1/storeId/:storeId/giftCard', storeController.handleGiftCardRequest);
+router.get('/api/v1/settings', storeController.handleSettingRequest);
 
 
 module.exports = router;
